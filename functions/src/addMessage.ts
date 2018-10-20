cc81bc8edf8168a2a4303f9f6a7cd45e6e04f9ea
Adrian Bunge <adrian@nonacreative.com>
2018-10-20 16:36:22 +0200

    First Commit

---
+++ functions/src/addMessage.ts
@@ -0,0 +1,61 @@
+import { DataSnapshot } from "firebase-functions/lib/providers/database";
+
+export default (functions, admin) => (data, context) => {
+    const message = data.message;
+    const sender_name = data.sender_name
+    const timestamp = data.timestamp
+    const chatId = data.chat_id
+    console.log(data)
+
+    const uid = context.auth.uid;
+
+    // Validations and security
+    if (!(typeof message === 'string') || message.length === 0) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+            'one arguments "text" containing the message text to add.');
+    }
+
+    if (!context.auth) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+            'while authenticated.');
+    }
+
+    admin.database().ref('/chat_members/' + chatId + '/' + context.auth.uid) .once('value', (snapshot:DataSnapshot) => {
+      if (!snapshot.exists()) { 
+        throw new functions.https.HttpsError('failed-precondition', 'The user must be a member of the chat.');
+      }
+  });
+
+
+
+
+    admin.database().ref('/chat_members/'+ chatId).once('value', (snapshot:DataSnapshot) => {
+        console.log('snapshot.numChildren' + snapshot.numChildren);
+        snapshot.forEach((childSnapshot:DataSnapshot) => {
+                  console.log(childSnapshot.key)
+                  return true;
+                });
+    });
+
+    return admin.database().ref('/messages').push({
+      message: message,
+      sender_uid: uid, 
+      sender_name: sender_name,
+      timestamp: timestamp,
+
+      
+    }).then(() => {
+      console.log('New Message written');
+      // Returning the sanitized message to the client.
+      return { message: message };
+    })
+    // [END returnMessageAsync]
+  //   .catch((error) => {
+  //     // Re-throwing the error as an HttpsError so that the client gets the error details.
+  //     throw new functions.https.HttpsError('unknown', error.message, error);
+  //   });
+    // [END_EXCLUDE]
+  };
+  // [END messageFunctionTrigger]
